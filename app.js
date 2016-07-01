34469d9f01597beddac93d6781d1a5d31e044d27
renzhao <3251qq@163.com>
2016-07-01 18:15:36 +0800

    自动新建文件夹

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ var request = require('request');
 var cheerio = require('cheerio');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 var app = express();
 
 app.use(express.static(path.join(__dirname)));
 app.use(bodyParser.json());
 
+//本地存储目录
+var dir = './img';
+
+//创建目录
+mkdirp(dir, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+
 var requrl = 'http://674976627.lofter.com/';
 var selecter = '.photo img';
 var attrs = 'src';
+
 app.get('/', function(req, res) {
+
     request(requrl, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(body);
@@ -41,7 +54,7 @@ app.get('/', function(req, res) {
 
     function download(uri, filename, callback) {
         request.head(uri, function(err, res, body) {
-            request(uri).pipe(fs.createWriteStream('img/' + filename)).on('close', callback);
+            request(uri).pipe(fs.createWriteStream(dir + '/' + filename)).on('close', callback);
         });
     }
 })
