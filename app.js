b79a1375e0b8cb00da64785655177eace8031650
renzhaozhao <3251qq@163.com>
2017-01-07 13:56:51 +0800

    不用express直接实现

--- app.js
+++ app.js
@@ -1,65 +1,88 @@
-var express = require('express');
-var utility = require('utility');
-var path = require('path');
-var request = require('request');
-var cheerio = require('cheerio');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var app = express();
+const http = require('http');
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
 
-app.use(express.static(path.join(__dirname)));
-app.use(bodyParser.json());
+// 设置被查询的目标网址
+const requrl = "http://www.haha.mx/topic/1/new/";
+const selecter = '.joke-list-item .joke-main-content a img';
+const attrs = 'src';
+const dir = './img/';
+const images = [];
+const maxpage = 2;
 
-//本地存储目录
-var dir = './img';
+start();
 
-//创建目录
-mkdirp(dir, function(err) {
-    if (err) {
-        console.log(err);
-    }
-});
-
-var requrl = 'http://674976627.lofter.com/';
-var selecter = '.photo img';
-var attrs = 'src';
-
-app.get('/', function(req, res) {
-
-    request(requrl, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(body);
-
-            var items = [];
-            $(selecter).each(function(idx, el) {
-                var src = $(el).attr(attrs);
-                var filename = qu(path.basename(src));
-
-                function qu(str) {
-                    return str.split('?')[0]
-                }
-                download(src, filename, function() {
-                    console.log(filename + ' is done');
-                })
-                items.push({
-                    img: src,
-                    base: filename
-                });
-            });
-
-            res.send(items);
+function start() {
+    console.log('开始新建目录');
+    mkdirp(dir, function(err) {
+        if (err) {
+            console.log(err);
         }
-    })
-
-    function download(uri, filename, callback) {
-        request.head(uri, function(err, res, body) {
-            request(uri).pipe(fs.createWriteStream(dir + '/' + filename)).on('close', callback);
-        });
+    });
+    console.log('开始获取图片链接');
+    for (let i = 1; i <= maxpage; i++) {
+        getImage(requrl, i);
     }
-})
+}
 
+function getImage(url, page) {
+    request(url + page, function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+            let $ = cheerio.load(body);
+            $(selecter).each(function(index, el) {
+                let src = $(el).attr(attrs).replace('/small/', '/big/');
+                let filename = path.basename(src);
+                if (src.indexOf('http://image.haha.mx') > -1) {
+                    images.push({
+                        src: src,
+                        filename: filename
+                    })
+                }
+            })
+            if (page === maxpage) {
+                let len = images.length;
+                console.log(`图片链接获取完毕！${len}`);
+                console.log(`链接总数量：${len}`);
+                if (len > 0) {
+                    downloadImage(images.shift());
+                }
+                else {
+                    console.log('下载完毕');
+                }
+            }
+        }
+    });
+}
 
-app.listen(3000, function() {
-    console.log('start in 3000')
-})
+function downloadImage(image, callback) {
+    var narr = image.src.replace("http://image.haha.mx/", "").split("/");
+    http.get(image.src, function(res) {
+        var imgData = "";
+        //一定要设置response的编码为binary否则会下载下来的图片打不开
+        res.setEncoding("binary");
+        res.on("data", function(chunk) {
+            imgData += chunk;
+        });
+        res.on('end', function() {
+            var savePath = dir + narr[0] + narr[1] + narr[2] + "_" + narr[4];
+            fs.writeFile(savePath + image.filename, imgData, 'binary', function(err) {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log(`${image.filename} 下载完成`);
+                    if (images.length > 0) {
+                        downloadImage(images.shift());
+                    }
+                    else {
+                        console.log('下载完毕');
+                    }
+                }
+            })
+        })
+
+    })
+}
